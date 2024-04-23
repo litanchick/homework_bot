3800229b21432b7e8d02b5077d434a7c0ace038e
litanchick <litanchick@yandex.ru>
2024-04-24 01:19:43 +0700

    fix first commit

--- homework.py
+++ homework.py
@@ -1,12 +1,12 @@
 import logging
 import os
-import sys
 import time
+import sys
+from http import HTTPStatus
 
 import requests
 import telegram
 from dotenv import load_dotenv
-from telegram.ext import Updater
 
 import exceptions
 
@@ -34,6 +34,14 @@ logging.basicConfig(
 )
 
 
+def check_message(message_last, message, bot):
+    """Проверяем новое ли сообщение, чтобы бот не дублировал информацию."""
+    if message_last != message:
+        message_last = message
+        send_message(bot, message_last)
+    return message_last
+
+
 def check_tokens():
     """Проверяем наличие всех данных в окружении."""
     list_tokens = [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]
@@ -43,54 +51,55 @@ def check_tokens():
 def send_message(bot, message):
     """Функция отправки сообщения при изменении статуса."""
     try:
+        logging.debug('Пытаемся отправить сообщение.')
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-        message = 'Сообщение успешно отправлено.'
-        logging.debug(message)
+        logging.debug('Сообщение успешно отправлено.')
     except telegram.error.TelegramError:
-        message = 'Не удалось отправить сообщение.'
-        raise exceptions.CantSendMessage(message)
+        raise exceptions.CantSendMessage(
+            'Не удалось отправить сообщение.'
+        )
 
 
 def get_api_answer(timestamp):
     """Получаем данные из ответа API."""
     try:
+        logging.debug('Пытаемся получить API ответ.')
         response = requests.get(
             ENDPOINT,
             headers=HEADERS,
             params=timestamp
         )
     except Exception as error:
-        message = f'API сервис недоступен: {error}.'
-        return message
-    if response.status_code == 200:
+        return f'API сервис недоступен: {error}.'
+    if response.status_code == HTTPStatus.OK:
         return response.json()
-    else:
-        raise Exception('Страница недоступна')
+    raise Exception('Страница недоступна')
 
 
 def check_response(response):
     """Проверяем ответ на соответствие формату данных."""
-    if type(response).__name__ == 'dict':
-        if 'homeworks' not in response:
-            raise exceptions.ValueDictNone(
-                'Данные не соответствуют запросу, нет названия работы.'
-            )
-        if not response['current_date']:
-            raise exceptions.ValueDictNone(
-                'Данные не соответствуют запросу, нет текущей даты.'
-            )
-        if type(response['homeworks']).__name__ != 'list':
-            message = 'Под ключом <homeworks> не список, как мы ожидали.'
-            raise TypeError(message)
-    else:
+    if not isinstance(response, dict):
         raise TypeError('Данные пришли не формате словаря, как мы ожидали.')
+    if 'homeworks' not in response:
+        raise exceptions.ValueDictNone(
+            'Данные не соответствуют запросу, нет названия работы.'
+        )
+    if not response['current_date']:
+        raise exceptions.ValueDictNone(
+            'Данные не соответствуют запросу, нет текущей даты.'
+        )
+    if type(response['homeworks']).__name__ != 'list':
+        raise TypeError(
+            'Под ключом <homeworks> не список, как мы ожидали.'
+        )
 
 
 def parse_status(homework):
     """Проверяем изменился ли статус проекта."""
     if not homework:
-        message = 'Словарь пришел пустой. А мы ожидали словарь с данными.'
-        raise ValueError(message)
+        raise ValueError(
+            'Словарь пришел пустой. А мы ожидали словарь с данными.'
+        )
 
     if 'homework_name' not in homework:
         raise exceptions.ValueDictNone(
@@ -113,8 +122,7 @@ def main():
             'В окружении не хватает токенов. Мне нужно больше токенов!!!!'
         )
         sys.exit(1)
-    message, error_check = '', ''
-    updater = Updater(token=TELEGRAM_TOKEN)
+    message_check, message, error_check = '', '', ''
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     while True:
@@ -122,24 +130,19 @@ def main():
             response = get_api_answer(timestamp)
             check_response(response)
             homework = response.get('homeworks')
-            message_check = parse_status(homework[0])
-            if message_check != message:
-                message = message_check
-                send_message(bot, message)
+            message = parse_status(homework[0])
         except Exception as error:
-            logging.error(error, exc_info=True)
-            if error_check != error:
-                error_check = error
-                try:
-                    send_message(bot, error_check)
-                except exceptions.CantSendMessage:
-                    logging.error(
-                        'Я не смог отправить тебе сообщение об ошибке.',
-                        exc_info=True
-                    )
-        time.sleep(RETRY_PERIOD)
-        updater.start_polling()
-        updater.idle()
+            error_check = check_message(error_check, error, bot)
+            logging.error(message, exc_info=True)
+        finally:
+            try:
+                message_check = check_message(message_check, message, bot)
+            except exceptions.CantSendMessage:
+                logging.error(
+                    'Я не смог отправить тебе сообщение об ошибке.',
+                    exc_info=True
+                )
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
